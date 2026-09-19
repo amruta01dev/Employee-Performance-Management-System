d7d98ce3e8723973f4663a28feffd114647555c2
amruta01dev <amrutashelke401@gmail.com>
2026-09-19 18:57:11 +0530

    Created Job Titles table and attendance status table

--- database/02_create_tables.sql
+++ database/02_create_tables.sql
@@ -1,7 +1,24 @@
 -- 01. Departments
 CREATE TABLE departments (
     department_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, --the text INTEGER GENERATED ALWAYS AS IDENTITY is used to automatically generate id for depratment
-    department_name VARCHAR(100) NOT NULL UNIQUE,
+    department_name VARCHAR(50) NOT NULL UNIQUE,
     department_description TEXT,
     created_date DATE NOT NULL DEFAULT CURRENT_DATE  -- the CURRENT_DATE automatically inserte the todays date by default if not provided  
 );
+
+-- 02. Job Titles
+CREATE TABLE job_titles (
+    job_title_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    job_title_name VARCHAR(50) NOT NULL,
+    job_level VARCHAR(50) NOT NULL
+);
+
+
+-- 03. Attendance Status
+CREATE TABLE attendance_status (
+    status_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    status_name VARCHAR(50) NOT NULL UNIQUE,
+    counts_as_present BOOLEAN NOT NULL DEFAULT FALSE,
+    counts_as_working_day BOOLEAN NOT NULL DEFAULT TRUE
+);
+
